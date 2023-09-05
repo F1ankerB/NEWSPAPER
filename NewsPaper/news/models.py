ca998bc70813513e88e7375bbd71cb274a5c1104
Nill Kiggers <artem.boyvan@mail.ru>
2023-09-05 12:17:12 +1000

    end commit

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,13 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
 class Author(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.FloatField(default=0.0)
+
+    def update_rating(self):
+        post_ratings = self.post_set.aggregate(total=Sum('rating'))['total'] * 3
+        comments_ratings = self.comment_set.aggregate(total=Sum('rating'))['total']
+        post_comment_ratings = Comment.objects.filter(post__author=self).aggregate(total=Sum('rating'))['total']
+
+        self.rating = post_ratings + comments_ratings + post_comment_ratings
+        self.save()
+
+
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100, unique=True)
+
+
 class Post(models.Model):
-    pass
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_type = models.CharField(max_length=50, choices=[('article', 'Article'), ('news', 'News')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.FloatField(default=0.0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[:124] + '...'
+
 class PostCategory(models.Model):
-    pass
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+
 class Comment(models.Model):
-    pass
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.FloatField(default=0.0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
 
 # Create your models here.
