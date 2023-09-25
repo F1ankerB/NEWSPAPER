2017f97b57e2314b9029158b8968779a2b3b918f
F1ankerB <136216045+F1ankerB@users.noreply.github.com>
2023-09-25 14:13:09 +1000

    create scheduler

--- NewsPaper/news/management/commands/runapscheduler.py
+++ NewsPaper/news/management/commands/runapscheduler.py
@@ -10,23 +10,40 @@ from django_apscheduler.models import DjangoJobExecution
 
 logger = logging.getLogger(__name__)
 
+from django.urls import reverse
 from django.core.mail import send_mail
-from news.models import Category
+from news.models import Category,Post
+from datetime import timedelta
+from django.utils import timezone
 
 def send_email_job():
     subject = 'Новые сообщения в вашей категории'
-    message = 'Здравствуйте, новые сообщения доступны в вашей категории.'
     from_email = 'te4kkaunt@yandex.ru'
 
     # Получите все категории с подписчиками
     categories_with_subscribers = Category.objects.filter(subscribers__isnull=False)
 
-    # Отправьте сообщение каждому подписчику в каждой категории
     for category in categories_with_subscribers:
         subscribers = category.subscribers.all()
+
+        # Получите все статьи за прошедшую неделю для этой категории
+        week_ago = timezone.now() - timedelta(days=7)
+        articles = Post.objects.filter(categories=category, created_at__gte=week_ago)
+
+        # Если статей нет, то пропускаем этот шаг
+        if not articles.exists():
+            continue
+
+        # Сформируйте сообщение
+        articles_info = []
+        for index, article in enumerate(articles, 1):
+            article_text = f"{index}. {article.title}\nТекст: {article.text[:50]}...\nСсылка: http://127.0.0.1:8000{article.get_absolute_url()}\n"
+            articles_info.append(article_text)
+
+        message_body = f'Здравствуйте, {subscribers[0].username},\n\nЗа прошедшую неделю были опубликованы следующие статьи:\n\n' + '\n'.join(articles_info)
+
         recipient_list = [user.email for user in subscribers]
-        send_mail(subject, message, from_email, recipient_list)
-# функция, которая будет удалять неактуальные задачи
+        send_mail(subject, message_body, from_email, recipient_list)
 def delete_old_job_executions(max_age=604_800):
     """This job deletes all apscheduler job executions older than `max_age` from the database."""
     DjangoJobExecution.objects.delete_old_job_executions(max_age)
@@ -42,7 +59,7 @@ class Command(BaseCommand):
         # добавляем работу нашему задачнику
         scheduler.add_job(
             send_email_job,
-            trigger=CronTrigger(second="0"),
+            trigger=CronTrigger(day_of_week="sun", hour="12", minute="0"),
             id="send_email_job",  # уникальный айди
             max_instances=1,
             replace_existing=True,
